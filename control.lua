d17936b712b9749fe9301a65792db101e92a274b
eugenikus8 <51211736+eugenikus8@users.noreply.github.com>
2024-10-27 10:31:12 +0200

    Update control.lua

--- control.lua
+++ control.lua
@@ -1,224 +1,124 @@
-﻿--control.lua
---This mod scans the map for cars and gun-turrets and places alerts when they are low on ammo.
+-- Constants
+local CHECK_INTERVAL = 600  -- 10 sec (600 t)
 
---util functions
+-- Checking the ammo level for an entity
+local function check_ammo(entity, ammo_threshold)
+    if entity and entity.valid then
+        local inventory
 
-function table_is_empty(table)
-    for _,_ in pairs(table) do
-        return false
+        -- Determining the type of inventory
+        if entity.type == "ammo-turret" or entity.type == "artillery-turret" then
+            inventory = entity.get_inventory(defines.inventory.turret_ammo)
+        elseif entity.type == "artillery-wagon" then
+            inventory = entity.get_inventory(defines.inventory.artillery_wagon_ammo)
+        elseif entity.type == "car" then
+            inventory = entity.get_inventory(defines.inventory.car_ammo)
+        elseif entity.type == "tank" then
+            inventory = entity.get_inventory(defines.inventory.tank_ammo)
+        elseif entity.type == "spider-vehicle" then
+            inventory = entity.get_inventory(defines.inventory.spider_ammo)
+        end
+
+        if inventory and #inventory > 0 then
+            local low_ammo = false
+            for i = 1, #inventory do
+                local ammo = inventory[i].count
+                if ammo == 0 then
+                    return "NO_AMMO"  -- No ammo in any slot
+                elseif ammo < ammo_threshold then
+                    low_ammo = true  -- At least one low ammo slot
+                end
+            end
+            if low_ammo then
+                return "LOW_AMMO"
+            end
+        end
     end
-    return true
+    return nil
 end
 
+-- Event handler for checking ammo level
+local function on_tick(event)
+    if event.tick % CHECK_INTERVAL ~= 0 then return end
 
---local functions
+    local ammo_threshold = settings.global["turret_alerts_low_ammo_threshold"].value
+    local turret_alerts_enabled = settings.global["turret_alerts_enabled"].value
+    local car_alerts_enabled = settings.global["turret_alerts_car_enabled"].value
+    local artillery_alerts_enabled = settings.global["turret_alerts_artillery_enabled"].value
+    local tank_alerts_enabled = settings.global["turret_alerts_tank_enabled"].value
+    local spidetron_alerts_enabled = settings.global["turret_alerts_spidetron_enabled"].value
 
-local get_ammo_flag = {
-	--Applies mode to inventory, calculates and returns respective inventory flags
-	--Uses boolean return value as ternary, with true = no ammo, false = low ammo and nil = enough ammo
-	["added"] = function (inventory,player_threshold)
-		if inventory.is_empty() then
-			return true
-		else
-			local ammo_count = 0
-			for i=1, #inventory do
-				ammo_count = ammo_count + inventory[i].count
-			end
-			if ammo_count == 0 then
-				return true
-			elseif ammo_count < player_threshold then
-				return false
-			end
-		end
-	end,
-	
-	["individually"] = function (inventory,player_threshold)
-		if inventory.is_empty() then
-			return true
-		else
-			for i=1, #inventory do
-				if inventory[i].count == 0 then
-					return true
-				elseif inventory[i].count < player_threshold then
-					return false
-				end
-			end
-		end
-	end,
+    for _, player in pairs(game.connected_players) do
+        local alerts = {}
+        for _, surface in pairs(game.surfaces) do
 
-	["selected"] = function (inventory,player_threshold,gun_index)
-		if inventory.is_empty() then
-			return true
-		else
-			if not gun_index then
-				-- default to first slot if no slot selected (probably multislot turret)
-				gun_index = 1
-			end
-			if inventory[gun_index].count == 0 then
-				return true
-			elseif inventory[gun_index].count < player_threshold then
-				return false
-			end
-		end
-	end
-}
+            -- Checking ammo levels for turrets
+            if turret_alerts_enabled then
+                for _, turret in pairs(surface.find_entities_filtered{type = {"ammo-turret", "artillery-turret"}}) do
+                    local status = check_ammo(turret, ammo_threshold)
+                    if status == "NO_AMMO" then
+                        table.insert(alerts, {entity = turret, icon = {type = "virtual", name = "no_ammo_signal"}, message = {"alerts.empty"}})
+                    elseif status == "LOW_AMMO" then
+                        table.insert(alerts, {entity = turret, icon = {type = "virtual", name = "low_ammo_signal"}, message = {"alerts.low"}})
+                    end
+                end
+            end
 
-local function add_entity_to_list(event)
-	--Whenever an ammo-turret, car or artillery type entity is built, add it to the global table.
-	local entity = event.created_entity or event.entity
-	local index = entity.surface.name.."_"..entity.force.name
-	if global.ammo_entities[index] then
-		table.insert(global.ammo_entities[index], entity)
-	end
+            -- Checking vehicle ammunition levels
+            if car_alerts_enabled then
+                for _, vehicle in pairs(surface.find_entities_filtered{type = "car"}) do
+                    local status = check_ammo(vehicle, ammo_threshold)
+                    if status == "NO_AMMO" then
+                        table.insert(alerts, {entity = vehicle, icon = {type = "virtual", name = "no_ammo_signal"}, message = {"alerts.empty"}})
+                    elseif status == "LOW_AMMO" then
+                        table.insert(alerts, {entity = vehicle, icon = {type = "virtual", name = "low_ammo_signal"}, message = {"alerts.low"}})
+                    end
+                end
+            end
+            
+            -- Checking the ammunition level for artillery wagons
+            if artillery_alerts_enabled then
+                for _, artillery in pairs(surface.find_entities_filtered{type = "artillery-wagon"}) do
+                    local status = check_ammo(artillery, ammo_threshold)
+                    if status == "NO_AMMO" then
+                        table.insert(alerts, {entity = artillery, icon = {type = "virtual", name = "no_ammo_signal"}, message = {"alerts.empty"}})
+                    elseif status == "LOW_AMMO" then
+                        table.insert(alerts, {entity = artillery, icon = {type = "virtual", name = "low_ammo_signal"}, message = {"alerts.low"}})
+                    end
+                end
+            end
+            
+            -- Checking tank ammunition levels
+            if tank_alerts_enabled then
+                for _, tank in pairs(surface.find_entities_filtered{type = "tank"}) do
+                    local status = check_ammo(tank, ammo_threshold)
+                    if status == "NO_AMMO" then
+                        table.insert(alerts, {entity = tank, icon = {type = "virtual", name = "no_ammo_signal"}, message = {"alerts.empty"}})
+                    elseif status == "LOW_AMMO" then
+                        table.insert(alerts, {entity = tank, icon = {type = "virtual", name = "low_ammo_signal"}, message = {"alerts.low"}})
+                    end
+                end
+            end
+
+            -- Checking Spidertron Ammo Levels
+            if spidetron_alerts_enabled then
+                for _, spidetron in pairs(surface.find_entities_filtered{type = "spider-vehicle"}) do
+                    local status = check_ammo(spidetron, ammo_threshold)
+                    if status == "NO_AMMO" then
+                        table.insert(alerts, {entity = spidetron, icon = {type = "virtual", name = "no_ammo_signal"}, message = {"alerts.empty"}})
+                    elseif status == "LOW_AMMO" then
+                        table.insert(alerts, {entity = spidetron, icon = {type = "virtual", name = "low_ammo_signal"}, message = {"alerts.low"}})
+                    end
+                end
+            end
+        end
+        
+        -- Display alerts for a player with a localized entity name
+        for _, alert in pairs(alerts) do
+            player.add_custom_alert(alert.entity, alert.icon, {"", alert.entity.localised_name, " ", alert.message}, true)
+        end
+    end
 end
 
-local function remove_entity_from_list(event)
-	--Whenever an ammo-turret, car or artillery type entity dies / is mined, remove it from the global table.
-	local index = event.entity.surface.name.."_"..event.entity.force.name
-	if global.ammo_entities[index] then
-		for i,entity in pairs(global.ammo_entities[index]) do
-			if (entity == event.entity) then
-				table.remove(global.ammo_entities[index], i)
-				break
-			end
-		end
-	end
-end
-
-local function add_force_to_list(event)
-	--Whenever a player of an unscanned force joins the game or a force is created, add all ammo-turret, car or artillery type entities of that force to the global table.
-	local player, force
-	if event.player_index then
-		player = game.get_player(event.player_index)
-		force = player.force
-	elseif event.force then
-		force = event.force
-		if force.connected_players then
-			player = force.connected_players[1]
-		end
-	end
-
-	if player and force and not global.ammo_entities[player.surface.name.."_"..force.name] then
-		for _,surface in pairs(game.surfaces) do
-			global.ammo_entities[surface.name.."_"..force.name] = surface.find_entities_filtered{type = {"ammo-turret","car","artillery-turret","artillery-wagon"}, force = force, to_be_deconstructed = false}
-		end
-	end
-end
-
-local function remove_force_from_list(event)
-	--Whenever the last player of a force leaves the game or forces are merged, remove all entities of that force from the global table.
-	local force
-	local param = {}
-	if event.player_index and event.force then
-		param.player_index = event.player_index
-		add_force_to_list(param)
-		force = event.force
-	elseif event.player_index then
-		force = game.get_player(event.player_index).force
-	elseif event.force then
-		force = event.force
-	elseif event.source and event.destination then
-		param.force = event.destination
-		add_force_to_list(param)
-		force = source
-	end
-
-	if force and not force.connected_players then
-		for surface_name,_ in pairs(game.surfaces) do
-			global.ammo_entities[surface_name.."_"..force.name] = nil
-		end
-	end
-end
-
-local function init_list()
-	-- index init
-	global.ammo_entities = {}
-	local param = {}
-	for _,force in pairs(game.forces) do
-		param.force = force
-		add_force_to_list(param)
-	end
-end
-
-local function generate_alerts()
-	--Every 10 seconds recheck and give alerts to players for ammo-turret, car or artillery type entities on the same force as them.
-	for _,player in pairs(game.connected_players) do
-		
-		local turret_enabled = player.mod_settings["gun-turret-alerts-enabled"].value
-		local car_enabled = player.mod_settings["gun-turret-alerts-car-enabled"].value
-		local artillery_enabled = player.mod_settings["gun-turret-alerts-artillery-enabled"].value
-		local mode = player.mod_settings["gun-turret-alerts-mode"].value
-		local player_threshold = player.mod_settings["gun-turret-alerts-threshold"].value
-		local ammo_entities = global.ammo_entities[player.surface.name.."_"..player.force.name]
-
-		if ammo_entities then
-			for _,entity in pairs(ammo_entities) do
-				if entity.valid and entity.force == player.force then
-
-					--Get ammo inventory based on entity type, skip cars without guns
-					local inventory
-					if turret_enabled and entity.type == "ammo-turret" then
-						inventory = entity.get_inventory(defines.inventory.turret_ammo)
-					elseif car_enabled and entity.type == "car" and not table_is_empty(entity.prototype.guns) then
-						inventory = entity.get_inventory(defines.inventory.car_ammo)
-					elseif artillery_enabled then
-						if entity.type == "artillery-turret" then
-							inventory = entity.get_inventory(defines.inventory.artillery_turret_ammo)
-						elseif entity.type == "artillery-wagon" then
-							inventory = entity.get_inventory(defines.inventory.artillery_wagon_ammo)
-						end
-					end
-
-					--Check for states of no or low ammo based on mode
-					local ammo_flag
-					if inventory and get_ammo_flag[mode] then
-						if entity.type == "ammo-turret" or entity.type == "artillery-turret" or entity.type == "artillery-wagon" then
-							ammo_flag = get_ammo_flag[mode](inventory, player_threshold)
-							if entity.prototype.automated_ammo_count then
-								if entity.prototype.automated_ammo_count < player_threshold then
-									ammo_flag = get_ammo_flag[mode](inventory, entity.prototype.automated_ammo_count)
-								end
-							end
-						elseif entity.type == "car" then
-							ammo_flag = get_ammo_flag[mode](inventory, player_threshold, entity.selected_gun_index)
-						end
-					end
-
-					--Create alert for present state
-					if ammo_flag then
-						-- no ammo alert
-						player.add_custom_alert(entity, {type = "virtual", name = "ammo-icon-red"}, {"gun-turret-alerts.message-empty", entity.localised_name}, true)
-					elseif ammo_flag == false then
-						-- low ammo alert
-						player.add_custom_alert(entity, {type = "virtual", name = "ammo-icon-yellow"}, {"gun-turret-alerts.message-low", entity.localised_name}, true)
-					end
-				end
-			end
-		end
-	end
-end
-
-
--- Event handlers
-
-script.on_init(init_list)
-script.on_configuration_changed(init_list)
-
-script.on_event(defines.events.on_player_joined_game, add_force_to_list)
-script.on_event(defines.events.on_player_left_game, remove_force_from_list)
-script.on_event(defines.events.on_player_changed_force, remove_force_from_list)
-
-script.on_event(defines.events.on_force_created, add_force_to_list)
-script.on_event(defines.events.on_forces_merged, remove_force_from_list)
-
-script.on_event(defines.events.on_built_entity, add_entity_to_list, {{filter="type", type = "ammo-turret"},{filter="type", type = "car"},{filter="type", type = "artillery-turret"},{filter="type", type = "artillery-wagon"}})
-script.on_event(defines.events.on_robot_built_entity, add_entity_to_list, {{filter="type", type = "ammo-turret"},{filter="type", type = "car"},{filter="type", type = "artillery-turret"},{filter="type", type = "artillery-wagon"}})
-script.on_event(defines.events.script_raised_built, add_entity_to_list, {{filter="type", type = "ammo-turret"},{filter="type", type = "car"},{filter="type", type = "artillery-turret"},{filter="type", type = "artillery-wagon"}})
-script.on_event(defines.events.script_raised_revive, add_entity_to_list, {{filter="type", type = "ammo-turret"},{filter="type", type = "car"},{filter="type", type = "artillery-turret"},{filter="type", type = "artillery-wagon"}})
-
-script.on_event(defines.events.on_player_mined_entity, remove_entity_from_list, {{filter="type", type = "ammo-turret"},{filter="type", type = "car"},{filter="type", type = "artillery-turret"},{filter="type", type = "artillery-wagon"}})
-script.on_event(defines.events.on_robot_mined_entity, remove_entity_from_list, {{filter="type", type = "ammo-turret"},{filter="type", type = "car"},{filter="type", type = "artillery-turret"},{filter="type", type = "artillery-wagon"}})
-script.on_event(defines.events.on_entity_died, remove_entity_from_list, {{filter="type", type = "ammo-turret"},{filter="type", type = "car"},{filter="type", type = "artillery-turret"},{filter="type", type = "artillery-wagon"}})
-script.on_event(defines.events.script_raised_destroy, remove_entity_from_list, {{filter="type", type = "ammo-turret"},{filter="type", type = "car"},{filter="type", type = "artillery-turret"},{filter="type", type = "artillery-wagon"}})
-
-script.on_nth_tick(600, generate_alerts)
+-- Registering the on_tick event
+script.on_event(defines.events.on_tick, on_tick)
