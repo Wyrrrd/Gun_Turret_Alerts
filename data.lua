5885e79e0ef939af061a1a4fe303b5a2966a2910
eugenikus8 <51211736+eugenikus8@users.noreply.github.com>
2024-10-27 10:31:49 +0200

    Update data.lua

--- data.lua
+++ data.lua
@@ -1,2 +1,15 @@
-﻿--data.lua
-require("prototypes.signals")
+data:extend(
+{
+    {
+        type = "virtual-signal",
+        name = "no_ammo_signal",
+        icon = "__core__/graphics/icons/alerts/ammo-icon-red.png",
+        icon_size = 64,
+    },
+    {
+        type = "virtual-signal",
+        name = "low_ammo_signal",
+        icon = "__core__/graphics/icons/alerts/warning-icon.png",
+        icon_size = 64,
+    }
+})
